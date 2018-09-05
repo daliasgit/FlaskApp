0b47576cf4b5ee259aa9c8cc547c11deae9de322
Dalia <umme.fatima@gmail.com>
2018-09-05 20:31:23 +0200

    Line 62, 143

--- catalog/__init__.py
+++ catalog/__init__.py
@@ -59,7 +59,7 @@ def fbconnect():
     app_id = json.loads(open('fb_client_secrets.json', 'r').read())[
         'web']['app_id']
     app_secret = json.loads(
-        open('fb_client_secrets.json', 'r').read())['web']['app_secret']
+        open('/var/www/FlaskApp/catalog/fb_client_secrets.json', 'r').read())['web']['app_secret']
     url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (app_id, app_secret, access_token)#noqa
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
